0737b8282ddf8364b37231c3c1410b2064359b29
Fiona Le Montreer <lemfi.spam@gmail.com>
2022-01-23 16:53:43 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
-var vitesse = 45
-
 //% color="#AA278D"
 namespace pilotage {
-
+    
+    //% block="vitesse"
+    export var vitesse = 45
+    
+    var distanceSensor = 100
+    
     //% block="aller à droite"
     export function aller_a_droite() {
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 13)
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 100 - 13)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, vitesseVirage())
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 100 - vitesseVirage())
+    }
+    
+    //% block="définir vitesse"
+    export function definir_vitesse(nouvelleVitesse) {
+        vitesse = nouvelleVitesse
+    }
+   
+    //% block="définir distance obstacle devant"
+    export function definir_vitesse(nouvelleDistance) {
+        distanceSensor = nouvelleDistance
     }
 
     //% block="obstacle à droite ?"
@@ -21,17 +34,21 @@ namespace pilotage {
 
     //% block="obstacle devant ?"
     export function obstacle_devant() {
-        return k_Bit.ultra() < 100
+        return k_Bit.ultra() < distanceSensor
     }
     
     //% block="aller à gauche"
     export function aller_a_gauche() {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 13)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 100 - 13)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, vitesseVirage())
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 100 - vitesseVirage())
     }
 
     //% block="avancer"
     export function avancer() {
         k_Bit.run(DIR.RunForward, vitesse)
+    }
+    
+    function vitesseVirage() {
+        return vitesse * 0,29
     }
 }
